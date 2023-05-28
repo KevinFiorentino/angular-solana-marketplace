5aebd256f677e5f671d5180caf16f9483dd2221d
Kevin <kevinfiorentino.1294@gmail.com>
2023-05-28 17:19:17 -0300

    Progress with forms

--- src/app/shared/components/@modals/add-nft-to-collection/add-nft-to-collection.component.ts
+++ src/app/shared/components/@modals/add-nft-to-collection/add-nft-to-collection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { SolanaNftService } from '@shared/services/solana-contracts/solana-nft.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -9,12 +10,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AddNftToCollectionComponent implements OnInit {
 
-  form: UntypedFormGroup = this.formBuilder.group({
+  public form: UntypedFormGroup = this.formBuilder.group({
     name: ['', Validators.required],
+    description: ['', Validators.required],
   });
+
+  public fileBuffer!: any;
+  public originalName = '';
+  public ext = '';
+  public errorImage = false;
 
   constructor(
     private formBuilder: UntypedFormBuilder,
+    private solanaNftService: SolanaNftService,
     private dialog: MatDialogRef<AddNftToCollectionComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
@@ -23,7 +31,28 @@ export class AddNftToCollectionComponent implements OnInit {
   }
 
   addNFT(): void {
+    this.errorImage = false;
+    if (!this.fileBuffer) {
+      this.errorImage = true;
+      return;
+    }
+    if (this.form.valid) {
 
+    }
+  }
+
+  onFileSelected(): void {
+    this.errorImage = false;
+    const inputNode: any = document.querySelector('#file');
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.fileBuffer = e.target.result;
+      };
+      this.originalName = inputNode.files[0].name;
+      this.ext = inputNode.files[0].name.split('.')[inputNode.files[0].name.split('.').length-1];
+      reader.readAsArrayBuffer(inputNode.files[0]);
+    }
   }
 
   closeModal(): void {
--- src/app/shared/components/@modals/new-collection/new-collection.component.ts
+++ src/app/shared/components/@modals/new-collection/new-collection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { SolanaNftService } from '@shared/services/solana-contracts/solana-nft.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -9,13 +10,20 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class NewCollectionComponent implements OnInit {
 
-  form: UntypedFormGroup = this.formBuilder.group({
+  public form: UntypedFormGroup = this.formBuilder.group({
     name: ['', Validators.required],
     symbol: ['', Validators.required],
+    description: ['', Validators.required],
   });
+
+  public fileBuffer!: any;
+  public originalName = '';
+  public ext = '';
+  public errorImage = false;
 
   constructor(
     private formBuilder: UntypedFormBuilder,
+    private solanaNftService: SolanaNftService,
     private dialog: MatDialogRef<NewCollectionComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
@@ -24,7 +32,28 @@ export class NewCollectionComponent implements OnInit {
   }
 
   newCollection(): void {
+    this.errorImage = false;
+    if (!this.fileBuffer) {
+      this.errorImage = true;
+      return;
+    }
+    if (this.form.valid) {
 
+    }
+  }
+
+  onFileSelected(): void {
+    this.errorImage = false;
+    const inputNode: any = document.querySelector('#file');
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.fileBuffer = e.target.result;
+      };
+      this.originalName = inputNode.files[0].name;
+      this.ext = inputNode.files[0].name.split('.')[inputNode.files[0].name.split('.').length-1];
+      reader.readAsArrayBuffer(inputNode.files[0]);
+    }
   }
 
   closeModal(): void {
