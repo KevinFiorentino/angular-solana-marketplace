f5801d5eb0596e9fa93acbc8fefe26af81e56aea
Kevin <kevinfiorentino.1294@gmail.com>
2023-05-31 22:06:08 -0300

    Change Infura env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   APP_URL: 'https://angular-solana-nft.vercel.app',
   infura: {
-    projectId: '2PQplgMb5WyKCNcJgu9oeNILVa8',
-    projectSecret: 'eecc04725abbd72e54b5d0765f3ade9b'
+    projectId: '2QaAqsJaNTeFqZpSSx1FOpsdWXR',
+    projectSecret: 'b80a9b782ca841b634fb923d2004c1d1'
   }
 };
